1baa2a68b36636d3c66197777f2f0bc1340ac8bc
vikasmaliya004 <vikasmaliya004@gmail.com>
2022-09-26 23:13:58 +0530

    change branch

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,24 @@
+// import { useEffect, useRef } from 'react'
+// import gsap from 'gsap-trial'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import LogoS from './vikas.jpg'
+import './index.scss'
+
+const Logo = () => {
+  
+  return (
+    <div className="logo-container" >
+      <div className='my-pic'>
+      <img
+        className="solid-logo"
+        src={LogoS}
+        alt="JavaScript,  Developer"
+      />
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Logo
